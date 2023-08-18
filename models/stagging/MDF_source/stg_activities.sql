743b8c483de61cfd1c2a727f6f0b032ccc79966e
manuel.arrubarrena@wizeline.com <manuel.arrubarrena@wizeline.com>
2023-08-18 19:24:12 +0000

    except

--- models/stagging/MDF_source/stg_activities.sql
+++ models/stagging/MDF_source/stg_activities.sql
@@ -1,6 +1,6 @@
 with
 source as (
-    SELECT User_id, in_vehicle, on_bicycle, on_foot, running, still, tilting, walking, unknown,
+    SELECT * EXCEPT (time),
     ROUND((CAST(SUBSTR(cast(time AS STRING), 6,2) AS FLOAT64)-50) / 0.83333, 2) AS hour_day
 
    FROM {{ source('Interview_data_analysis_MDF', 'MDF_activities') }}
