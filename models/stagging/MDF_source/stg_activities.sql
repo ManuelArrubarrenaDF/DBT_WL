3fcde0e4f3ac4aae597a1b62724b18b288bbc8a3
manuel.arrubarrena@wizeline.com <manuel.arrubarrena@wizeline.com>
2023-08-18 21:04:45 +0000

    hour ch

--- models/stagging/MDF_source/stg_activities.sql
+++ models/stagging/MDF_source/stg_activities.sql
@@ -1,7 +1,7 @@
 with
 source as (
     SELECT * EXCEPT (time),
-    ROUND((CAST(SUBSTR(cast(time AS STRING), 6,2) AS FLOAT64)-50) / 0.83333, 2) AS hour_day
+    ROUND((CAST(SUBSTR(cast(time AS STRING), 6,2) AS FLOAT64)-50) / 0.416666, 2) AS hour_day
 
    FROM {{ source('Interview_data_analysis_MDF', 'MDF_activities') }}
 )
