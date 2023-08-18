9d2b094a4980c8a5aae782c0d0678d531e05ff01
manuel.arrubarrena@wizeline.com <manuel.arrubarrena@wizeline.com>
2023-08-18 01:08:06 +0000

    more

---
+++ models/stagging/MDF_source/stg_appusage.sql
@@ -0,0 +1,27 @@
+with
+source as (
+    SELECT User_id, total_fg_time,
+   CASE 
+        WHEN LOWER(package_name) LIKE '%android%' THEN 'android'
+        WHEN LOWER(package_name) LIKE '%whatsapp%' THEN 'whatsapp'
+        WHEN LOWER(package_name) LIKE '%mixplorer%' THEN 'mixplorer'
+        WHEN LOWER(package_name) LIKE '%spotify%' THEN 'spotify'
+        WHEN LOWER(package_name) LIKE '%vodafone%' THEN 'vodafone'
+        WHEN LOWER(package_name) LIKE '%telegram%' THEN 'telegram'
+        WHEN LOWER(package_name) LIKE '%sensapp%' THEN 'sensapp'
+        WHEN LOWER(package_name) LIKE '%rsoft%' THEN 'rsoft'
+        WHEN LOWER(package_name) LIKE '%argosoft%' THEN 'argosoft'
+        WHEN LOWER(package_name) LIKE '%minecraftpe%' THEN 'minecraftpe'
+        WHEN LOWER(package_name) LIKE '%robtopx%' THEN 'robtopx'
+        WHEN LOWER(package_name) LIKE '%supercell%' THEN 'supercell'
+        WHEN LOWER(package_name) LIKE '%studio8apps%' THEN 'studio8apps'
+   ELSE package_name
+   END AS pack_name_clean
+
+   FROM {{ source('Interview_data_analysis_MDF', 'MDF_appusage') }}
+)
+
+SELECT User_id, SUM (total_fg_time) AS time_in_app, pack_name_clean
+FROM source
+GROUP BY pack_name_clean, User_id
+ORDER BY time_in_app DESC
