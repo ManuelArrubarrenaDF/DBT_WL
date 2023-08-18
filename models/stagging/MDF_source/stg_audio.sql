b1ef18963326d3ecbeaa4455a70fe8f99bf9fd43
manuel.arrubarrena@wizeline.com <manuel.arrubarrena@wizeline.com>
2023-08-18 21:36:10 +0000

    audio

---
+++ models/stagging/MDF_source/stg_audio.sql
@@ -0,0 +1,10 @@
+with
+source as (
+    SELECT * EXCEPT (time),
+    ROUND((CAST(SUBSTR(cast(time AS STRING), 6,2) AS FLOAT64)-50) / 0.416666, 2) AS hour_day
+
+   FROM {{ source('Interview_data_analysis_MDF', 'MDF_audio') }}
+)
+
+SELECT *
+FROM source
