fd52e65020f39fce81a810619e7e7f38304b59d0
manuel.arrubarrena@wizeline.com <manuel.arrubarrena@wizeline.com>
2023-08-25 20:15:23 +0000

    commit

--- models/marts/core/T_appusage.sql
+++ models/marts/core/T_appusage.sql
@@ -1,4 +1,4 @@
-SELECT User_id, SUM (total_fg_time) AS time_in_app, pack_name_clean, hour_day
+SELECT User_id, SUM (total_fg_time) AS time_in_app, pack_name_clean, hours, minutes, hour_min
 FROM {{ ref('stg_appusage') }}
-GROUP BY  pack_name_clean, User_id, hour_day
+GROUP BY  pack_name_clean, User_id, hours, minutes, hour_min
 ORDER BY time_in_app DESC
