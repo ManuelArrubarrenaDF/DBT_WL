0411252fbfe8a178cd743a3a151839380b00ef99
manuel.arrubarrena@wizeline.com <manuel.arrubarrena@wizeline.com>
2023-09-27 22:51:25 +0000

    chngs

--- models/stagging/MDF_source/Dim_time.sql
+++ models/stagging/MDF_source/Dim_time.sql
@@ -1,9 +1,13 @@
 {%- set table_names = ['activities', 'apps_usage', 'audio', 'battery', 'bt_scan','calls','cells','display','installed_apps','location','running_apps','wifi_sacns'] -%}
 
-{% for table_na in table_names -%}
-SELECT time 
-FROM {{ source('IDA_MDF_raw', '{{ table_na }}') }}
-
+{% for table_names in table_names -%}
+    SELECT time 
+    FROM {{ source('IDA_MDF_raw', '{{table_na}}') }}
+    --FROM `data-eng-training-87b25bc6.IDA_MDF_raw.{{table_names}}`
 {%- if not loop.last -%}
      UNION ALL
    {% endif -%}
+
+{%- endfor %}
+
+--FROM {{ source('Interview_data_analysis_MDF', 'MDF_activities') }}
