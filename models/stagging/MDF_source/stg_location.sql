9acc39a742dda07fe32d3e36dee88c30633a0849
manuel.arrubarrena@wizeline.com <manuel.arrubarrena@wizeline.com>
2023-10-03 19:38:04 +0000

    done stg_

--- models/stagging/MDF_source/stg_location.sql
+++ models/stagging/MDF_source/stg_location.sql
@@ -1,25 +1,10 @@
-/*with
+with
 source as (
-    SELECT * EXCEPT (time),
-    date(TIMESTAMP_MILLIS(time)) as date_t, 
-    CASE 
-    WHEN EXTRACT(HOUR FROM TIMESTAMP_MILLIS(time)) <= 11 THEN 'morning'
-    WHEN EXTRACT(HOUR FROM TIMESTAMP_MILLIS(time)) > 11 AND EXTRACT(HOUR FROM TIMESTAMP_MILLIS(time)) <= 14 THEN 'noon'
-    WHEN EXTRACT(HOUR FROM TIMESTAMP_MILLIS(time)) > 14 AND EXTRACT(HOUR FROM TIMESTAMP_MILLIS(time)) <= 19 THEN 'afternoon'
-    WHEN EXTRACT(HOUR FROM TIMESTAMP_MILLIS(time)) > 19 THEN 'night'
-    END AS hours,
-    --CAST(CONCAT(CAST(EXTRACT(HOUR FROM TIMESTAMP_MILLIS(time)) AS STRING), CAST((EXTRACT(HOUR FROM TIMESTAMP_MILLIS(time))/1) AS STRING)) AS FLOAT64) AS hour_minute,
-    ROUND(EXTRACT(HOUR FROM TIMESTAMP_MILLIS(time)) + (EXTRACT(MINUTE FROM TIMESTAMP_MILLIS(time))) / 100, 2) AS hour_min,
-    TIMESTAMP_MILLIS(time) - TIMESTAMP_MILLIS(postime) AS dif_pos_time,
+    SELECT * 
 
-    CASE 
-    WHEN EXTRACT(DAYOFWEEK FROM TIMESTAMP_MILLIS(time)) >= 6 THEN 'weekend'
-    ELSE 'weekday' END AS Day_of_week 
-
-   FROM {{ source('Interview_data_analysis_MDF', 'MDF_location') }}
+   FROM {{ source('IDA_MDF_raw', 'location') }}
 )
 
 SELECT * EXCEPT(dif_pos_time, speed, acc, alt, bearing, postime),
 EXTRACT(MINUTE FROM dif_pos_time) AS dif_minutes_post,
 FROM source
-*/
