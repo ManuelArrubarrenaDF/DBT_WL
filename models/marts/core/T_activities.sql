9e72f6eb02d8ef7f269aaa29347f774bdae8fd01
manuel.arrubarrena@wizeline.com <manuel.arrubarrena@wizeline.com>
2023-10-04 16:13:11 +0000

    activities

--- models/marts/core/T_activities.sql
+++ models/marts/core/T_activities.sql
@@ -1,4 +1,5 @@
-SELECT User_id, 
+SELECT User_id,
+time, 
 SUM (in_vehicle) as sum_vehicle,
 SUM(on_bicycle) as sum_bicicle,
 SUM(on_foot) as sum_foot,
@@ -7,7 +8,7 @@ SUM(still)as sum_still,
 SUM(tilting) as sum_tilting,
 SUM(walking) as sum_walking,
 SUM(unknown) as sum_unknown,
-date_t, hours, hour_min, Day_of_week
+--date_t, hours, hour_min, Day_of_week
 FROM {{ ref('stg_activities') }}
-GROUP BY date_t, hours, hour_min, Day_of_week, User_id
+GROUP BY User_id, time
 --ORDER BY hour_day DESC
